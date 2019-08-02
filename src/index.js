683aac76f3d8d92abc8ca54eb57b0432ca6b5761
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2019-08-02 13:58:05 -0400

    looks good

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export { default as Header } from "src/Header";
-export { default as FillSpan } from "src/FillSpan";
+export { default as Header } from "./Header";
+export { default as FillSpan } from "./FillSpan";
+export { default as Description } from "./Description";
+export { default as Link } from "./Link";
+export { default as ListSection } from "./ListSection";
